ddd4f59c0bd47950c1836269837e645170b55940
leocourbassier <leocourbassier@hotmail.com>
2017-05-26 19:34:22 -0300

    First commit

---
+++ source/elf-loader/.avprintf-cb.o.d
@@ -0,0 +1,4 @@
+avprintf-cb.o: /home/leonardo/dce-quagga/source/elf-loader/avprintf-cb.c \
+ /usr/include/stdc-predef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
+ /home/leonardo/dce-quagga/source/elf-loader/avprintf-cb.h
